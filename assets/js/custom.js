85252ba6ddfb7a2ba26f11cdb2a69dcbd68cf1ff
qwerewqwerew <qwerew0@gmail.com>
2023-02-05 11:51:33 +0900

    dd

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,12 @@
 $(function () {
 	let wW=$(window).width();
+	let wH=$(window).heignt();
 	$("img").css("width", "300px");	
 	$("img").each(function (idx, el) {
 		$(el).click(function () {
 			let tg = $(this);
 			if (!tg.hasClass('close')) {
-				tg.css({width:(wW*.7)+"px", height:"auto",position:"fixed","z-index":5,left:"50%",top:"50%",transform:"translate(-50%,-50%)"});
+				tg.css({width:"auto", height:(wW*.7)+"px",position:"fixed","z-index":5,left:"50%",top:"50%",transform:"translate(-50%,-50%)"});
 				tg.addClass('close');
 			}else{
 				tg.parent().css({"position":"static"});
